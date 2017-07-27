ed13ea1e128434ea7fff299feaa805defd7ea6af
everton.hilario <evertonjuru@gmail.com>
2017-07-27 00:52:39 -0300

    #2 Finalizada a Seção 3 do curso. Foi criada uma estrutura mínima de um portal de notícia

--- app.js
+++ app.js
@@ -5,25 +5,36 @@ var express = require('express');
 // executa a função contida na variável express
 var app = express();
 
+//apoonta o modulo ejs para a enginer de view
+app.set('view engine', 'ejs');
+
 // configura a rota da home
 app.get('/', function(req, res){
 
-	// resposta em html
-	// Obs. ver que diferente do node puro usa send invés de end
-	res.send('<html><body>Home page</body></html>');
+	//renderiza o html (home/index.ejs)
+	res.render('home/index');
+
 });
 
 // configura a rota seção /tecnologia
-app.get('/tecnologia', function(req, res){
+app.get('/formulario_inclusao_noticia', function(req, res){
 
-	// resposta em html
-	// Obs. ver que diferente do node puro usa send invés de end
-	res.send('<html><body>Tecnologia page</body></html>');
+	//renderiza o html (secao/tecnologia.ejs)
+	res.render('admin/form_add_noticia');
+
+});
+
+// configura a rota seção /tecnologia
+app.get('/noticias', function(req, res){
+
+	//renderiza o html (secao/tecnologia.ejs)
+	res.render('noticias/noticias');
+
 });
 
 // soobe e configura o servidor
-// var porta int
-// var callback function
+// argumento porta int
+// argumento callback function
 app.listen(3000, function(){
 
 	console.log("Server rodando com express");
