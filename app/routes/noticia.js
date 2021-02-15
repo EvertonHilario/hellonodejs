7a2a035ef9c346f70f2910bea52d70c5a69e791a
evertonhilario <evertonjuru@gmail.com>
2021-02-14 22:12:52 -0300

    feat: Refactoring do projeto prático portal de notícias - implementando classes

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -2,24 +2,12 @@ module.exports = function(application){
 
 	// configura a rota seção /noticias
 	application.get('/noticia', function(req, res){
-
-		// executa o método que conecta ao banco
 		var connection = application.config.dbConnection();
 		
-		// instancia noticiasModel
-		var noticiasModel = application.app.models.noticiasModel;
+		var noticiasDAO = new application.app.models.NoticiasDAO(connection);
 
-		// executa a função getNoticias que está na model noticiaModel
-		// var connection = conexão com o banco
-		// var callback = função que será executada apoós a consulta
-		// obs ref callback: A função de callbbak neste caso espera dois parâmetros o erro e o resultado.
-		noticiasModel.getNoticia(connection, function(error, result){
-
-			//renderiza o html (noticias/noticias.ejs)
-			// passa como parametro json o resultado da consulta
-			// noticias = result
+		noticiasDAO.getNoticia(function(error, result){
 			res.render('noticias/noticia', {noticia : result});
-
 		})
 
 	});
